672d0a4f804505e0ab4bbf060085f4bb8a7c1149
Joao Pedro <jp.bruno.machado@gmail.com>
2025-03-01 00:26:08 -0300

    removed wrong url

--- src/axios/axiosBase.ts
+++ src/axios/axiosBase.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-    baseURL: process.env.NODE_ENV === "development" ? "http://localhost:4000" : "https://api.starmap.orionarquitetura.com.br",
+    baseURL: process.env.NODE_ENV === "development" ? "http://localhost:4000" : "",
     headers: {"Content-Type": "application/json"},
     withCredentials: true,
 })
 
 export const axiosBaseFileUpload = axios.create({
-    baseURL: process.env.NODE_ENV === "development" ? "http://localhost:4000" : "https://api.starmap.orionarquitetura.com.br",
+    baseURL: process.env.NODE_ENV === "development" ? "http://localhost:4000" : "",
     headers: {"Content-Type": "multipart/form-data"},
     withCredentials: true,
 })
