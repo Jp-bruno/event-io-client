ba184bec2135b280603d62385bd1a0076fb27e6f
Joao Pedro <jp.bruno.machado@gmail.com>
2025-03-01 15:19:53 -0300

    server url

--- src/axios/axiosBase.ts
+++ src/axios/axiosBase.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-    baseURL: process.env.NODE_ENV === "development" ? "http://localhost:4000" : "https://event-io-server.onrender.com",
+    baseURL: process.env.NODE_ENV === "development" ? "http://localhost:4000" : "https://api.jp-bruno.dev",
     headers: {"Content-Type": "application/json"},
     withCredentials: true,
 })
 
 export const axiosBaseFileUpload = axios.create({
-    baseURL: process.env.NODE_ENV === "development" ? "http://localhost:4000" : "https://event-io-server.onrender.com",
+    baseURL: process.env.NODE_ENV === "development" ? "http://localhost:4000" : "https://api.jp-bruno.dev",
     headers: {"Content-Type": "multipart/form-data"},
     withCredentials: true,
 })
